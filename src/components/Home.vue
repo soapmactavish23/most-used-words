5454332af14aa8282c75b422f5ecd9b623875a0f
HKprogram <ricknogueira1231@gmail.com>
2021-08-24 17:25:10 -0300

    a

---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container fluid>
+    <v-form>
+      <v-file-input multiple chips v-model="files"></v-file-input>
+    </v-form>
+    <div class="pills">
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill.vue";
+export default {
+  components: { Pill },
+  data: function () {
+    return {
+      groupedWords: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
